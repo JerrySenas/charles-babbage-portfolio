dbd1bb7c0871d799d2d578cf1cfcfa5414e17d22
jerrysenas1 <jerrysenas1@gmail.com>
2025-10-02 18:33:15 +0800

    Initial commit

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,17 @@
+<script setup>
+  defineProps({
+    project: Object
+  });
+</script>
+
+<template>
+  <div class="card col-md-3">
+    <img :src="project.image" class="card-img-top card-img" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">{{ project.title }}</h5>
+      <p class="card-text">
+        {{ project.description }}
+      </p>
+    </div>
+  </div>
+</template>
